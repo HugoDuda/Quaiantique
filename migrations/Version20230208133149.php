3a4d0e807bbb5abeef915e36b01b2ac53063df27
Hugo_DUDA <hugo.duda@protonmail.com>
2023-02-08 16:06:03 +0100

    Image

---
+++ migrations/Version20230208133149.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230208133149 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE image DROP index_image_size, CHANGE index_image_name index_image_file VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE image ADD index_image_size INT NOT NULL, CHANGE index_image_file index_image_name VARCHAR(255) NOT NULL');
+    }
+}
